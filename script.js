9fd104713045c73c2ec797115bc413f2b9e630e3
DEBAYAN MUKHERJEE <mdebayan1999@gmail.com>
2024-03-20 00:29:13 -0700

    change

--- script.js
+++ script.js
@@ -1,16 +1,4 @@
-var Result=document.getElementById('result');
-var count=0;
-document.getElementById('btn').addEventListener("click",()=>{
-    count++;
-    if(count<10){
-       
-        Result.textContent= "0" + count;
-    }
-    
-       
-    
-})
-document.body.style.backgroundColor="green";
+
 
 
 
